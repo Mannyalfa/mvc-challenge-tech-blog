26143fd200687a60af2a702f9f537207f56900dd
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-08-01 00:13:00 -0400

    resoved logout issue

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,25 +1,14 @@
-async function loginFormHandler(event) {
-    event.preventDefault();
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
     }
 }
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('#logout').addEventListener('click', logout);
